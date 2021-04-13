4c928db61a5c672b4315210a8e60991dd3f9b03e
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-13 14:59:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MusicExcelOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            MusicFileInfo[] musicFileInfos = FileFetcher.GetMusicFiles(currentDirectory);
+
+            ExcelOperations.WriteToExcelFile(musicFileInfos);
+        }
+    }
+}
