4181298d5bc7e3c462f5a6d18f2507f2a9f4e839
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-14 19:54:04 +0530

    Wrapped the main program in try catch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MusicExcelOrganizer.Models;
+using MusicExcelOrganizer.Utils;
+using System;
+using System.Linq;
 
 namespace MusicExcelOrganizer
 {
@@ -6,10 +9,18 @@ namespace MusicExcelOrganizer
     {
         static void Main(string[] args)
         {
-            string currentDirectory = Environment.CurrentDirectory;
-            MusicFileInfo[] musicFileInfos = FileFetcher.GetMusicFiles(currentDirectory);
+            try
+            {
+                string currentDirectory = Environment.CurrentDirectory;
+                string folderName = currentDirectory.Split('\\').Last();
 
-            ExcelOperations.WriteToExcelFile(musicFileInfos);
+                MusicFileInfo[] musicFileInfos = FileFetcher.GetMusicFiles(currentDirectory);
+                ExcelHelper.WriteToExcelFile(musicFileInfos, folderName);
+            } 
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Program encountered an error: ${exception}. Please report the error on Github: https://github.com/anikait1/MusicExcelOrganizer/");
+            }
         }
     }
 }
