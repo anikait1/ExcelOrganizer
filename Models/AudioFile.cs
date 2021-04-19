bf07afb787af8f7417f7ff783d1ae91fbeeef1fa
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-19 19:30:24 +0530

    Interface to indicate how object will be serialized in a excel sheet

---
+++ Models/AudioFile.cs
@@ -0,0 +1,71 @@
+﻿using ATL;
+using ClosedXML.Excel;
+using System;
+using System.IO;
+
+namespace MusicExcelOrganizer.Models
+{
+    /// <summary>
+    /// Stores information about each song track. Information is constructed
+    /// using available Tags and File Metadata.
+    /// </summary>
+    public class AudioFile : IExcelSerializer
+    {
+        #region Song Information 
+        public string Title { get; init; }
+        public string Album { get; init; }
+        public string Genre { get; init; }
+        public int Year { get; init; }
+        public TimeSpan Length { get; init; }
+        #endregion
+
+        #region Artist Information
+        public string ContributingArtists { get; init; }
+        public string AlbumArtists { get; init; }
+        public string Composers { get; init; }
+        #endregion
+
+        #region File Information
+        public string Name { get; init; }
+        public string Path { get; init; }
+        public string Type { get; init; }
+        public DateTime DateModified { get; init; }
+        #endregion
+
+        public AudioFile(Track track, FileInfo fileInfo)
+        {
+            Title = string.IsNullOrWhiteSpace(track.Title) ? fileInfo.Name : track.Title;
+            Album = track.Album;
+            Genre = track.Genre;
+            Year = track.Year;
+            Length = TimeSpan.FromMilliseconds(track.DurationMs);
+
+            ContributingArtists = track.Artist;
+            AlbumArtists = track.AlbumArtist;
+            Composers = track.Composer;
+
+            Name = fileInfo.Name;
+            Path = fileInfo.FullName;
+            Type = track.AudioFormat.Name;
+            DateModified = fileInfo.LastWriteTime;
+        }
+
+        public void WriteToExcelRow(IXLWorksheet worksheet, int row)
+        {
+            worksheet.Cell(row, 1).Value = Name;
+            worksheet.Cell(row, 2).Value = Title;
+            worksheet.Cell(row, 3).Value = ContributingArtists;
+            worksheet.Cell(row, 4).Value = Year;
+
+            worksheet.Cell(row, 5).Value = Genre;
+            worksheet.Cell(row, 6).Value = Album;
+            worksheet.Cell(row, 7).Value = Path;
+            worksheet.Cell(row, 8).Value = AlbumArtists;
+
+            worksheet.Cell(row, 9).Value = DateModified;
+            worksheet.Cell(row, 10).Value = Length;
+            worksheet.Cell(row, 11).Value = Type;
+            worksheet.Cell(row, 12).Value = Composers;
+        }
+    }
+}
---
+++ Models/IExcelSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicExcelOrganizer.Models
+{
+    /// <summary>
+    /// Classes implementing this interface can be serialized to an Excel worksheet
+    /// </summary>
+    public interface IExcelSerializer
+    {
+        public void WriteToExcelRow(ClosedXML.Excel.IXLWorksheet worksheet, int row);
+    }
+}
