6c16a33fa4c707e537e2438e3e72462e9e788d0e
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-19 19:31:50 +0530

    under construction

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using MusicExcelOrganizer.Models;
 using MusicExcelOrganizer.Utils;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace MusicExcelOrganizer
@@ -12,14 +14,47 @@ namespace MusicExcelOrganizer
             try
             {
                 string currentDirectory = Environment.CurrentDirectory;
+                currentDirectory = @"C:\Users\Anikait\Downloads";
                 string folderName = currentDirectory.Split('\\').Last();
+                string category = "all";
 
-                MusicFileInfo[] musicFileInfos = FileFetcher.GetMusicFiles(currentDirectory);
-                ExcelHelper.WriteToExcelFile(musicFileInfos, folderName);
+                var profile = Profile.GetProfile(category);
+                (HashSet<string> extensions, string[] titles, Func<string, IExcelSerializer> map) = Profile.GetProfile(category);
+
+                var data = FileFetcher.GetFiles(currentDirectory, extensions)
+                                      .SelectSkipExceptions(file => map(file))
+                                      .ToArray();
+
+                ExcelWriter.WriteToExcelFile(folderName, titles, data);
             } 
             catch (Exception exception)
             {
                 Console.WriteLine($"Program encountered an error: ${exception}. Please report the error on Github: https://github.com/anikait1/MusicExcelOrganizer/");
+            }
+        }
+    }
+
+    static class LinqExtensions
+    {
+        public static IEnumerable<TResult> SelectSkipExceptions<TSource, TResult>(
+            this IEnumerable<TSource> values,
+            Func<TSource, TResult> selector)
+        {
+            foreach (var item in values)
+            {
+                TResult output;
+                try
+                {
+                    Console.WriteLine($"Processing {item}");
+                    output = selector(item);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Previous file is skipped. Error: {exception.Message}");
+                    continue;
+                }
+
+                yield return output;
             }
         }
     }
