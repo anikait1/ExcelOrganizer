21466daddc16031890b8ea7e5f45527b57a435b6
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-20 16:02:05 +0530

    Added a custom select extension, which skips the exceptions

---
+++ Utils/LinqExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicExcelOrganizer.Utils
+{
+
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TResult> SelectSkipExceptions<TSource, TResult>(
+            this IEnumerable<TSource> values,
+            Func<TSource, TResult> selector)
+        {
+            foreach (var item in values)
+            {
+                TResult output;
+                try
+                {
+                    Console.WriteLine($"Processing {item}");
+                    output = selector(item);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Previous file is skipped. Error: {exception.Message}");
+                    continue;
+                }
+
+                yield return output;
+            }
+        }
+    }
+}
