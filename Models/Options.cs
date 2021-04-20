9bbed0b07effe7f1d0355a86a377170b4cc10121
Anikait <50035624+anikait1@users.noreply.github.com>
2021-04-20 16:01:44 +0530

    Encapsulates the command line options available

---
+++ Models/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicExcelOrganizer.Models
+{
+    /// <summary>
+    /// Holds the different options that are made available through the command line
+    /// interface.
+    /// </summary>
+    public class Options
+    {
+        [Option('c', "category", Required = true, HelpText = "Category of files: [all, audio]")]
+        public FileCategory Category { get; set; }
+
+        [Option('d', "directory", Required = true, HelpText = "Directory to be scanned. By default it is set to the directory where program is being executed")]
+        public string Directory { get; set; } = Environment.CurrentDirectory;
+    }
+}
